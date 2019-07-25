e79dcad38721302826829973f0a6838e8b6db85d
iwHiteRabbiT <jc.cavadini@gmail.com>
2019-07-25 21:47:00 +1000

    TrackerBot

---
+++ CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STrackerBot.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ASTrackerBot::ASTrackerBot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	meshComp->SetCanEverAffectNavigation(false);
+	RootComponent = meshComp;
+}
+
+// Called when the game starts or when spawned
+void ASTrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASTrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
