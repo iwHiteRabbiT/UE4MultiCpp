c71ac44f59d49380b5937587ccc5afa14b9a8e4e
iwHiteRabbiT <jc.cavadini@gmail.com>
2019-08-07 16:58:43 +1000

    EQS Spawn AI

---
+++ CoopGame/Source/CoopGame/Public/SGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SGameMode.generated.h"
+
+using namespace UP;
+using namespace UC;
+using namespace UM;
+using namespace UF;
+
+UCLASS()
+class COOPGAME_API ASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
